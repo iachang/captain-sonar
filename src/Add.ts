a10033f2ebdef8ef02a5887ebeff6148ab7501b6
Satvik1701 <satvik.sharma@berkeley.edu>
2023-04-20 18:19:31 -0700

    simple move

--- src/Add.ts
+++ src/Add.ts
@@ -1,4 +1,4 @@
-import { Field, SmartContract, state, State, method } from 'snarkyjs';
+import { Field, SmartContract, state, State, method, Poseidon, Character } from 'snarkyjs';
 
 /**
  * Basic Example
@@ -10,17 +10,58 @@ import { Field, SmartContract, state, State, method } from 'snarkyjs';
  * This file is safe to delete and replace with your own contract.
  */
 export class Add extends SmartContract {
-  @state(Field) num = State<Field>();
+  @state(Field) P1x = State<Field>();
+  @state(Field) P1y = State<Field>();
+  @state(Field) P2x = State<Field>();
+  @state(Field) P2y = State<Field>();
 
   init() {
     super.init();
-    this.num.set(Field(1));
   }
 
-  @method update() {
-    const currentState = this.num.get();
-    this.num.assertEquals(currentState); // precondition that links this.num.get() to the actual on-chain state
-    const newState = currentState.add(2);
-    this.num.set(newState);
+  @method init_position(salt:Field, player:Field, x: Field, y: Field) {
+    if (player.equals(Field.fromNumber(1))) {
+      this.P1x.set(Poseidon.hash([ salt, x ]));
+      this.P1y.set(Poseidon.hash([ salt, y ]));
+    } else {
+      this.P2x.set(Poseidon.hash([ salt, x ]));
+      this.P2y.set(Poseidon.hash([ salt, y ]));
+    }
+  }
+
+  @method update(direction: Character, salt: Field, player:Field) {
+    var dx = 0;
+    var dy = 0;
+    if (direction.equals(Character.fromAscii('N'))) {
+      dy = 1;
+    } else if (direction.equals(Character.fromAscii('S'))) {
+      dy = -1;
+    } else if (direction.equals(Character.fromAscii('E'))) {
+      dx = 1;
+    } else if (direction.equals(Character.fromAscii('W'))) {
+      dx = -1;
+    } else {
+      // throw error
+    }
+
+    if (player.equals(Field.fromNumber(1))) {
+      const curr_P1x = this.P1x.get();
+      const curr_P1y = this.P1y.get();
+      this.P1x.assertEquals(curr_P1x);
+      this.P1y.assertEquals(curr_P1y);
+      const new_P1x = curr_P1x.add(Field.fromNumber(dx));
+      const new_P1y = curr_P1y.add(Field.fromNumber(dy));
+      this.P1x.set(new_P1x);
+      this.P1y.set(new_P1y);
+    } else {
+      const curr_P2x = this.P2x.get();
+      const curr_P2y = this.P2y.get();
+      this.P2x.assertEquals(curr_P2x);
+      this.P2y.assertEquals(curr_P2y);
+      const new_P2x = curr_P2x.add(Field.fromNumber(dx));
+      const new_P2y = curr_P2y.add(Field.fromNumber(dy));
+      this.P2x.set(new_P2x);
+      this.P2y.set(new_P2y);
+    }
   }
 }
