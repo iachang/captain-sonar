65b11243b139f5196c7a84eaef88cb82932fac95
System Administrator <root@Ians-MacBook-Pro.local>
2023-04-24 01:34:20 -0700

    fix

--- src/CapSonar.ts
+++ src/CapSonar.ts
@@ -1,4 +1,12 @@
-import { Field, SmartContract, state, State, method, Poseidon, Character } from 'snarkyjs';
+import {
+  Field,
+  SmartContract,
+  state,
+  State,
+  method,
+  Poseidon,
+  Character,
+} from 'snarkyjs';
 
 /**
  * Basic Example
@@ -19,38 +27,38 @@ export class CapSonar extends SmartContract {
     super.init();
   }
 
-  @method init_position(salt:Field, player:Field, x: Field, y: Field) {
-    if (player.equals(Field.fromNumber(1))) {
-      this.P1x.set(Poseidon.hash([ salt, x ]));
-      this.P1y.set(Poseidon.hash([ salt, y ]));
+  @method init_position(salt: Field, player: Field, x: Field, y: Field) {
+    if (player.equals(Field(1))) {
+      this.P1x.set(Poseidon.hash([salt, x]));
+      this.P1y.set(Poseidon.hash([salt, y]));
     } else {
-      this.P2x.set(Poseidon.hash([ salt, x ]));
-      this.P2y.set(Poseidon.hash([ salt, y ]));
+      this.P2x.set(Poseidon.hash([salt, x]));
+      this.P2y.set(Poseidon.hash([salt, y]));
     }
   }
 
-  @method update_pos(direction: Character, salt: Field, player:Field) {
-    var dx = 0;
-    var dy = 0;
-    if (direction.equals(Character.fromAscii('N'))) {
+  @method update_pos(direction: Character, salt: Field, player: Field) {
+    let dx = 0;
+    let dy = 0;
+    if (direction.equals(Character.fromString('N'))) {
       dy = 1;
-    } else if (direction.equals(Character.fromAscii('S'))) {
+    } else if (direction.equals(Character.fromString('S'))) {
       dy = -1;
-    } else if (direction.equals(Character.fromAscii('E'))) {
+    } else if (direction.equals(Character.fromString('E'))) {
       dx = 1;
-    } else if (direction.equals(Character.fromAscii('W'))) {
+    } else if (direction.equals(Character.fromString('W'))) {
       dx = -1;
     } else {
       // throw error
     }
 
-    if (player.equals(Field.fromNumber(1))) {
+    if (player.equals(Field(1))) {
       const curr_P1x = this.P1x.get();
       const curr_P1y = this.P1y.get();
       this.P1x.assertEquals(curr_P1x);
       this.P1y.assertEquals(curr_P1y);
-      const new_P1x = curr_P1x.add(Field.fromNumber(dx));
-      const new_P1y = curr_P1y.add(Field.fromNumber(dy));
+      const new_P1x = curr_P1x.add(Field(dx));
+      const new_P1y = curr_P1y.add(Field(dy));
       this.P1x.set(new_P1x);
       this.P1y.set(new_P1y);
     } else {
@@ -58,19 +66,18 @@ export class CapSonar extends SmartContract {
       const curr_P2y = this.P2y.get();
       this.P2x.assertEquals(curr_P2x);
       this.P2y.assertEquals(curr_P2y);
-      const new_P2x = curr_P2x.add(Field.fromNumber(dx));
-      const new_P2y = curr_P2y.add(Field.fromNumber(dy));
+      const new_P2x = curr_P2x.add(Field(dx));
+      const new_P2y = curr_P2y.add(Field(dy));
       this.P2x.set(new_P2x);
       this.P2y.set(new_P2y);
     }
   }
 
   @method get_pos(player: Field) {
-      if (player.equals(Field.fromNumber(1))) {
-          return [this.P1x.get(), this.P1y.get()];
-      } else {
-          return [this.P2x.get(), this.P2y.get()];
-      }
+    if (player.equals(Field(1))) {
+      return [this.P1x.get(), this.P1y.get()];
+    } else {
+      return [this.P2x.get(), this.P2y.get()];
+    }
   }
-
 }
