52621294f4f1953e7f7a27d4ceccd93ae0910ca2
System Administrator <root@Ians-MacBook-Pro.local>
2023-05-05 18:07:56 -0700

    fixed and added tests

--- src/CapSonar.ts
+++ src/CapSonar.ts
@@ -16,12 +16,16 @@ export class CapSonar extends SmartContract {
   @state(Field) P1y = State<Field>(); // Hidden
   @state(Field) P2x = State<Field>(); // Hidden
   @state(Field) P2y = State<Field>(); // Hidden
-  @state(Field) P1health = State<Field>(); // Not Hidden
-  @state(Field) P2health = State<Field>(); // Not Hidden
-  @state(Field) P1_submerge_step = State<Field>(); // Not Hidden
-  @state(Field) P2_submerge_step = State<Field>(); // Not Hidden
   @state(Field) step = State<Field>(); // Not Hidden
   @state(Field) size = State<Field>(); // Not Hidden
+
+  // ISSUE: we can have at most 8 field elements!!!
+  @state(Field) P1health = State<Field>(); // Not Hidden
+  @state(Field) P2health = State<Field>(); // Not Hidden
+  /*
+  @state(Field) P1_submerge_step = State<Field>(); // Not Hidden
+  @state(Field) P2_submerge_step = State<Field>(); // Not Hidden
+
   @state(Field) P1attackedatX = State<Field>(); // Not Hidden
   @state(Field) P1attackedatY = State<Field>(); // Not Hidden
   @state(Field) P2attackedatX = State<Field>(); // Not Hidden
@@ -29,7 +33,7 @@ export class CapSonar extends SmartContract {
   @state(Field) P1attacked = State<Field>(); // Not Hidden
   @state(Field) P2attacked = State<Field>(); // Not Hidden
   @state(Field) P1_last_attack = State<Field>(); // Not Hidden
-  @state(Field) P2_last_attack = State<Field>(); // Not Hidden
+  @state(Field) P2_last_attack = State<Field>(); // Not Hidden*/
 
   init() {
     super.init();
@@ -43,24 +47,24 @@ export class CapSonar extends SmartContract {
     this.P2health.set(Poseidon.hash([salt, health]));
   }
 
-  @method p2_init_position(salt:Field, x: Field, y: Field) {
+  @method p2_init_position(salt: Field, x: Field, y: Field) {
     this.P2x.set(Poseidon.hash([salt, x]));
     this.P2y.set(Poseidon.hash([salt, y]));
   }
 
-  @method p1_init_position(salt:Field, x: Field, y: Field) {
-    this.P1x.set(Poseidon.hash([ salt, x ]));
-    this.P1y.set(Poseidon.hash([ salt, y ]));
+  @method p1_init_position(salt: Field, x: Field, y: Field) {
+    this.P1x.set(Poseidon.hash([salt, x]));
+    this.P1y.set(Poseidon.hash([salt, y]));
   }
 
   @method init_board(size: Field) {
     this.step.set(Field(0));
-    this.P1_submerge_step.set(Field(0));
-    this.P2_submerge_step.set(Field(0));
+    //this.P1_submerge_step.set(Field(0));
+    // this.P2_submerge_step.set(Field(0));
     this.size.set(size);
   }
 
-  @method update_p1_pos(direction: Field, xc :Field, yc:Field, salt: Field) {
+  @method update_p1_pos(direction: Field, xc: Field, yc: Field, salt: Field) {
     // N, E, S, W
 
     const d1 = direction.sub(Field(1));
@@ -90,7 +94,7 @@ export class CapSonar extends SmartContract {
     this.P1y.set(Poseidon.hash([salt, yc.add(ymove)]));
   }
 
-  @method update_p2_pos(direction: Field, xc :Field, yc:Field, salt: Field) {
+  @method update_p2_pos(direction: Field, xc: Field, yc: Field, salt: Field) {
     // N, E, S, W
     // have to make sure we don't leave the board with size
 
@@ -130,27 +134,65 @@ export class CapSonar extends SmartContract {
   // }
 
   //check the x and y is within the board size
-  @method check_valid_pos(x: Field, y:Field) {
+  @method check_valid_pos(x: Field, y: Field) {
+    // you need to input the raw x and y fields
     const size = this.size.get();
-    const valid = x.lessThan(size).and(y.lessThan(size)).and(x.greaterThanOrEqual(Field(0))).and(y.greaterThanOrEqual(Field(0)));
-    return valid;
+    this.size.assertEquals(size);
+    x.assertLessThan(size);
+    y.assertLessThan(size);
+    x.assertGreaterThanOrEqual(Field(0));
+    y.assertGreaterThanOrEqual(Field(0));
+    //const valid = x.lessThan(size).and(y.lessThan(size)).and(x.greaterThanOrEqual(Field(0))).and(y.greaterThanOrEqual(Field(0)));
+    //return valid;
   }
 
   //wait 5 steps to attack again
   @method p1_attack_p2(x: Field, y: Field) {
-    const attack = Circuit.if(this.step.get().sub(this.P1_last_attack.get()).greaterThanOrEqual(5), Field(1), Field(0));
-    this.P1_last_attack.set(Circuit.if(attack.equals(Field(1)), this.step.get(), this.P1_last_attack.get()));
-    this.P2attackedatX.set(Circuit.if(attack.equals(Field(1)), x, this.P2attackedatX.get()));
-    this.P2attackedatY.set(Circuit.if(attack.equals(Field(1)), y, this.P2attackedatY.get()));
-    this.P2attacked.set(Circuit.if(attack.equals(Field(1)), Field(1), this.P2attacked.get()));
+    const attack = Circuit.if(
+      this.step.get().sub(this.P1_last_attack.get()).greaterThanOrEqual(5),
+      Field(1),
+      Field(0)
+    );
+    this.P1_last_attack.set(
+      Circuit.if(
+        attack.equals(Field(1)),
+        this.step.get(),
+        this.P1_last_attack.get()
+      )
+    );
+    this.P2attackedatX.set(
+      Circuit.if(attack.equals(Field(1)), x, this.P2attackedatX.get())
+    );
+    this.P2attackedatY.set(
+      Circuit.if(attack.equals(Field(1)), y, this.P2attackedatY.get())
+    );
+    this.P2attacked.set(
+      Circuit.if(attack.equals(Field(1)), Field(1), this.P2attacked.get())
+    );
   }
 
   @method p2_attack_p1(x: Field, y: Field) {
-    const attack = Circuit.if(this.step.get().sub(this.P2_last_attack.get()).greaterThanOrEqual(5), Field(1), Field(0));
-    this.P2_last_attack.set(Circuit.if(attack.equals(Field(1)), this.step.get(), this.P2_last_attack.get()));
-    this.P1attackedatX.set(Circuit.if(attack.equals(Field(1)), x, this.P1attackedatX.get()));
-    this.P1attackedatY.set(Circuit.if(attack.equals(Field(1)), y, this.P1attackedatY.get()));
-    this.P1attacked.set(Circuit.if(attack.equals(Field(1)), Field(1), this.P1attacked.get()));
+    const attack = Circuit.if(
+      this.step.get().sub(this.P2_last_attack.get()).greaterThanOrEqual(5),
+      Field(1),
+      Field(0)
+    );
+    this.P2_last_attack.set(
+      Circuit.if(
+        attack.equals(Field(1)),
+        this.step.get(),
+        this.P2_last_attack.get()
+      )
+    );
+    this.P1attackedatX.set(
+      Circuit.if(attack.equals(Field(1)), x, this.P1attackedatX.get())
+    );
+    this.P1attackedatY.set(
+      Circuit.if(attack.equals(Field(1)), y, this.P1attackedatY.get())
+    );
+    this.P1attacked.set(
+      Circuit.if(attack.equals(Field(1)), Field(1), this.P1attacked.get())
+    );
   }
 
   /**
@@ -160,7 +202,7 @@ export class CapSonar extends SmartContract {
    * @param y The y position of the p1
    * @returns  2 if the player is attacked at the exact position x and y and 1 if the player is attacked at a position within 1 manhattan distance of x and y
    */
-  @method p1_check_if_attacked(x: Field, y: Field, salt:Field) {
+  @method p1_check_if_attacked(x: Field, y: Field, salt: Field) {
     const curr_P1health = this.P1health.get();
     this.P1health.assertEquals(curr_P1health);
 
@@ -176,21 +218,30 @@ export class CapSonar extends SmartContract {
     const xadd1 = this.P1attackedatX.get().add(1);
     const yadd1 = this.P1attackedatY.get().add(1);
 
-    const cond1 = x.equals(this.P1attackedatX.get()).and(y.equals(this.P1attackedatY.get()))
-    const cond2 = x.equals(xsub1).and(y.equals(this.P1attackedatY.get()))
-    const cond3 = x.equals(xadd1).and(y.equals(this.P1attackedatY.get()))
-    const cond4 = x.equals(this.P1attackedatX.get()).and(y.equals(ysub1))
-    const cond5 = x.equals(this.P1attackedatX.get()).and(y.equals(yadd1))
+    const cond1 = x
+      .equals(this.P1attackedatX.get())
+      .and(y.equals(this.P1attackedatY.get()));
+    const cond2 = x.equals(xsub1).and(y.equals(this.P1attackedatY.get()));
+    const cond3 = x.equals(xadd1).and(y.equals(this.P1attackedatY.get()));
+    const cond4 = x.equals(this.P1attackedatX.get()).and(y.equals(ysub1));
+    const cond5 = x.equals(this.P1attackedatX.get()).and(y.equals(yadd1));
     const damage = Circuit.switch(
-      [cond1, cond2, cond3, cond4, cond5, (cond1.or(cond2).or(cond3).or(cond4).or(cond5)).not()],
+      [
+        cond1,
+        cond2,
+        cond3,
+        cond4,
+        cond5,
+        cond1.or(cond2).or(cond3).or(cond4).or(cond5).not(),
+      ],
       Field,
       [Field(2), Field(1), Field(1), Field(1), Field(1), Field(0)]
     );
-    this.P1health.set(curr_P1health.sub(damage.mul(this.P1attacked.get())))
+    this.P1health.set(curr_P1health.sub(damage.mul(this.P1attacked.get())));
     this.P1attacked.set(Field(0));
   }
 
-  @method p2_check_if_attacked(x: Field, y: Field, salt:Field) {
+  @method p2_check_if_attacked(x: Field, y: Field, salt: Field) {
     const curr_P2health = this.P2health.get();
     this.P2health.assertEquals(curr_P2health);
 
@@ -206,17 +257,26 @@ export class CapSonar extends SmartContract {
     const xadd1 = this.P2attackedatX.get().add(1);
     const yadd1 = this.P2attackedatY.get().add(1);
 
-    const cond1 = x.equals(this.P2attackedatX.get()).and(y.equals(this.P2attackedatY.get()))
-    const cond2 = x.equals(xsub1).and(y.equals(this.P2attackedatY.get()))
-    const cond3 = x.equals(xadd1).and(y.equals(this.P2attackedatY.get()))
-    const cond4 = x.equals(this.P2attackedatX.get()).and(y.equals(ysub1))
-    const cond5 = x.equals(this.P2attackedatX.get()).and(y.equals(yadd1))
+    const cond1 = x
+      .equals(this.P2attackedatX.get())
+      .and(y.equals(this.P2attackedatY.get()));
+    const cond2 = x.equals(xsub1).and(y.equals(this.P2attackedatY.get()));
+    const cond3 = x.equals(xadd1).and(y.equals(this.P2attackedatY.get()));
+    const cond4 = x.equals(this.P2attackedatX.get()).and(y.equals(ysub1));
+    const cond5 = x.equals(this.P2attackedatX.get()).and(y.equals(yadd1));
     const damage = Circuit.switch(
-      [cond1, cond2, cond3, cond4, cond5, (cond1.or(cond2).or(cond3).or(cond4).or(cond5)).not()],
+      [
+        cond1,
+        cond2,
+        cond3,
+        cond4,
+        cond5,
+        cond1.or(cond2).or(cond3).or(cond4).or(cond5).not(),
+      ],
       Field,
       [Field(2), Field(1), Field(1), Field(1), Field(1), Field(0)]
     );
-    this.P2health.set(curr_P2health.sub(damage.mul(this.P2attacked.get())))
+    this.P2health.set(curr_P2health.sub(damage.mul(this.P2attacked.get())));
     this.P2attacked.set(Field(0));
   }
 
@@ -224,7 +284,14 @@ export class CapSonar extends SmartContract {
    * The player submerges into the water and can move 3 steps in the next turn
    * This special power is activated after 15 turns
    */
-  @method p1_submerge(curr_x: Field, curr_y: Field, step1:Field, step2:Field, step3:Field, salt: Field) {
+  @method p1_submerge(
+    curr_x: Field,
+    curr_y: Field,
+    step1: Field,
+    step2: Field,
+    step3: Field,
+    salt: Field
+  ) {
     const d1 = step1.sub(Field(1));
     const d2 = step1.sub(Field(2));
     const d3 = step1.sub(Field(3));
@@ -275,21 +342,46 @@ export class CapSonar extends SmartContract {
       [Field(1), Field(0), Field(-1), Field(0)]
     );
 
-    const submerge = Circuit.if(this.step.get().sub(this.P1_submerge_step.get()).greaterThanOrEqual(15), Field(1), Field(0));
-    this.P1_submerge_step.set(Circuit.if(submerge.equals(Field(1)), this.step.get(), this.P1_submerge_step.get()));
-    const dx = Circuit.if(submerge.equals(Field(1)), step1x.add(step2x).add(step3x), Field(0));
-    const dy = Circuit.if(submerge.equals(Field(1)), step1y.add(step2y).add(step3y), Field(0));
+    const submerge = Circuit.if(
+      this.step.get().sub(this.P1_submerge_step.get()).greaterThanOrEqual(15),
+      Field(1),
+      Field(0)
+    );
+    this.P1_submerge_step.set(
+      Circuit.if(
+        submerge.equals(Field(1)),
+        this.step.get(),
+        this.P1_submerge_step.get()
+      )
+    );
+    const dx = Circuit.if(
+      submerge.equals(Field(1)),
+      step1x.add(step2x).add(step3x),
+      Field(0)
+    );
+    const dy = Circuit.if(
+      submerge.equals(Field(1)),
+      step1y.add(step2y).add(step3y),
+      Field(0)
+    );
     const curr_P1x = this.P1x.get();
     this.P1x.assertEquals(curr_P1x);
-    Poseidon.hash([ salt, curr_x ]).assertEquals(curr_P1x);
-    this.P1x.set(Poseidon.hash([ salt, curr_P1x.add(Field(dx)) ]));
+    Poseidon.hash([salt, curr_x]).assertEquals(curr_P1x);
+    this.P1x.set(Poseidon.hash([salt, curr_P1x.add(Field(dx))]));
     const curr_P1y = this.P1y.get();
     this.P1y.assertEquals(curr_P1y);
-    Poseidon.hash([ salt, curr_y ]).assertEquals(curr_P1y);
-    this.P1y.set(Poseidon.hash([ salt, curr_P1y.add(Field(dy)) ]));    
+    Poseidon.hash([salt, curr_y]).assertEquals(curr_P1y);
+    this.P1y.set(Poseidon.hash([salt, curr_P1y.add(Field(dy))]));
   }
 
-  @method p2_submerge(curr_x: Field, curr_y: Field, step1:Field, step2:Field, step3:Field, salt: Field) {
+  @method p2_submerge(
+    curr_x: Field,
+    curr_y: Field,
+    step1: Field,
+    step2: Field,
+    step3: Field,
+    salt: Field
+  ) {
     const d1 = step1.sub(Field(1));
     const d2 = step1.sub(Field(2));
     const d3 = step1.sub(Field(3));
@@ -338,17 +430,35 @@ export class CapSonar extends SmartContract {
       Field,
       [Field(1), Field(0), Field(-1), Field(0)]
     );
-    const submerge = Circuit.if(this.step.get().sub(this.P2_submerge_step.get()).greaterThanOrEqual(15), Field(1), Field(0));
-    this.P2_submerge_step.set(Circuit.if(submerge.equals(Field(1)), this.step.get(), this.P2_submerge_step.get()));
-    const dx = Circuit.if(submerge.equals(Field(1)), step1x.add(step2x).add(step3x), Field(0));
-    const dy = Circuit.if(submerge.equals(Field(1)), step1y.add(step2y).add(step3y), Field(0));
+    const submerge = Circuit.if(
+      this.step.get().sub(this.P2_submerge_step.get()).greaterThanOrEqual(15),
+      Field(1),
+      Field(0)
+    );
+    this.P2_submerge_step.set(
+      Circuit.if(
+        submerge.equals(Field(1)),
+        this.step.get(),
+        this.P2_submerge_step.get()
+      )
+    );
+    const dx = Circuit.if(
+      submerge.equals(Field(1)),
+      step1x.add(step2x).add(step3x),
+      Field(0)
+    );
+    const dy = Circuit.if(
+      submerge.equals(Field(1)),
+      step1y.add(step2y).add(step3y),
+      Field(0)
+    );
     const curr_P2x = this.P2x.get();
     this.P2x.assertEquals(curr_P2x);
-    Poseidon.hash([ salt, curr_x ]).assertEquals(curr_P2x);
-    this.P2x.set(Poseidon.hash([ salt, curr_P2x.add(Field(dx)) ]));
+    Poseidon.hash([salt, curr_x]).assertEquals(curr_P2x);
+    this.P2x.set(Poseidon.hash([salt, curr_P2x.add(Field(dx))]));
     const curr_P2y = this.P2y.get();
     this.P2y.assertEquals(curr_P2y);
-    Poseidon.hash([ salt, curr_y ]).assertEquals(curr_P2y);
-    this.P2y.set(Poseidon.hash([ salt, curr_P2y.add(Field(dy)) ]));
+    Poseidon.hash([salt, curr_y]).assertEquals(curr_P2y);
+    this.P2y.set(Poseidon.hash([salt, curr_P2y.add(Field(dy))]));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {
   Field,
   Mina,
   PrivateKey,
+  Poseidon,
   Character,
   AccountUpdate,
 } from 'snarkyjs';
@@ -22,7 +23,7 @@ const { privateKey: deployerKey, publicKey: deployerAccount } =
 const { privateKey: senderKey, publicKey: senderAccount } =
   Local.testAccounts[1];
 
-const salt = Field.random();
+let salt = Field.random();
 
 // ----------------------------------------------------
 
@@ -36,6 +37,7 @@ const deployTxn = await Mina.transaction(deployerAccount, () => {
   zkAppInstance.deploy();
   zkAppInstance.p1_init_position(salt, Field(0), Field(0));
   zkAppInstance.p2_init_position(salt, Field(0), Field(0));
+  zkAppInstance.init_board(Field(100));
 });
 await deployTxn.prove();
 await deployTxn.sign([deployerKey, zkAppPrivateKey]).send();
@@ -66,21 +68,103 @@ while (true) {
 }*/
 
 // get the initial state of IncrementSecret after deployment
-const num0 = zkAppInstance.P1x.get();
-console.log('state after init:', num0.toString());
+const numx0 = zkAppInstance.P1x.get();
+const numy0 = zkAppInstance.P1y.get();
+console.log('(x, y) after init:', numx0.toString(), numy0.toString());
+
+Poseidon.hash([salt, Field(0)]).assertEquals(numx0);
+Poseidon.hash([salt, Field(0)]).assertEquals(numy0);
 
 // ----------------------------------------------------
 
 const txn1 = await Mina.transaction(senderAccount, () => {
-  zkAppInstance.update_p1_pos(Field(1),Field(0),Field(0), salt);
+  zkAppInstance.update_p1_pos(Field(1), Field(0), Field(0), salt);
 });
 
 await txn1.prove();
 await txn1.sign([senderKey]).send();
 
-const num1 = zkAppInstance.P1x.get();
-const num2 = zkAppInstance.P1y.get();
-console.log('(x, y) after txn1:', num1.toString(), num2.toString());
+const numx1 = zkAppInstance.P1x.get();
+const numy1 = zkAppInstance.P1y.get();
+console.log('(x, y) after txn1:', numx1.toString(), numy1.toString());
+
+Poseidon.hash([salt, Field(0)]).assertEquals(numx1);
+Poseidon.hash([salt, Field(1)]).assertEquals(numy1);
+
+// ----------------------------------------------------
+
+const txn2 = await Mina.transaction(senderAccount, () => {
+  zkAppInstance.update_p1_pos(Field(2), Field(0), Field(1), salt);
+});
+
+await txn2.prove();
+await txn2.sign([senderKey]).send();
+
+const numx2 = zkAppInstance.P1x.get();
+const numy2 = zkAppInstance.P1y.get();
+console.log('(x, y) after txn2:', numx2.toString(), numy2.toString());
+
+Poseidon.hash([salt, Field(1)]).assertEquals(numx2);
+Poseidon.hash([salt, Field(1)]).assertEquals(numy2);
+
+// ----------------------------------------------------
+
+const txn3 = await Mina.transaction(senderAccount, () => {
+  zkAppInstance.update_p1_pos(Field(3), Field(1), Field(1), salt);
+});
+
+await txn3.prove();
+await txn3.sign([senderKey]).send();
+
+const numx3 = zkAppInstance.P1x.get();
+const numy3 = zkAppInstance.P1y.get();
+console.log('(x, y) after txn3:', numx3.toString(), numy3.toString());
+
+Poseidon.hash([salt, Field(1)]).assertEquals(numx3);
+Poseidon.hash([salt, Field(0)]).assertEquals(numy3);
+
+// ----------------------------------------------------
+
+const txn4 = await Mina.transaction(senderAccount, () => {
+  zkAppInstance.update_p1_pos(Field(4), Field(1), Field(0), salt);
+});
+
+await txn4.prove();
+await txn4.sign([senderKey]).send();
+
+const numx4 = zkAppInstance.P1x.get();
+const numy4 = zkAppInstance.P1y.get();
+console.log('(x, y) after txn4', numx4.toString(), numy4.toString());
+
+Poseidon.hash([salt, Field(0)]).assertEquals(numx4);
+Poseidon.hash([salt, Field(0)]).assertEquals(numy4);
+
+// ----------------------------------------------------
+
+const txn5 = await Mina.transaction(senderAccount, () => {
+  zkAppInstance.check_valid_pos(Field(0), Field(0));
+});
+
+await txn5.prove();
+await txn5.sign([senderKey]).send();
+
+// ----------------------------------------------------
+
+const txn6 = await Mina.transaction(senderAccount, () => {
+  zkAppInstance.update_p1_pos(Field(3), Field(0), Field(0), salt);
+});
+
+await txn6.prove();
+await txn6.sign([senderKey]).send();
+
+const numx5 = zkAppInstance.P1x.get();
+const numy5 = zkAppInstance.P1y.get();
+console.log('(x, y) after txn6', numx5.toString(), numy5.toString());
+
+Poseidon.hash([salt, Field(0)]).assertEquals(numx5);
+Poseidon.hash([salt, Field(-1)]).assertEquals(numy5);
+
+// ----------------------------------------------------
 
 // ----------------------------------------------------
 
